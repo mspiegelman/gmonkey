0db4da2140c9815fa030ac69cf3264dccfc91dc0
Maxwell Spiegelman <maxwellspiegelman@gmail.com>
2024-10-19 01:04:02 -0400

    Convert indentation to spaces

--- utils/waitForElements.js
+++ utils/waitForElements.js
@@ -3,35 +3,35 @@
 const FOUND_ATTR = 'data-userscript-alreadyFound';
 
 const getTargetNodes = selector => {
-    if (selector === undefined) return [];
-	if (typeof selector === 'function') return (selector() || []).map(n => [selector.name, n]);
-	if (typeof selector === 'string') return [...document.querySelectorAll(selector)].map(n => [selector, n]);
-    if (selector.every(s => document.querySelectorAll(s).length > 0)) return selector.flatMap(s => [...document.querySelectorAll(s)].map(n => [s, n]));
-    return [];
+  if (selector === undefined) return [];
+  if (typeof selector === 'function') return (selector() || []).map(n => [selector.name, n]);
+  if (typeof selector === 'string') return [...document.querySelectorAll(selector)].map(n => [selector, n]);
+  if (selector.every(s => document.querySelectorAll(s).length > 0)) return selector.flatMap(s => [...document.querySelectorAll(s)].map(n => [s, n]));
+  return [];
 };
 
 const waitForKeyElements = (
-	selectorOrFunction, // A selector string, a list of selector strings, or a function.
-	callback,			// The callback function; Takes a dom element and a selector string as parameters. If it returns true, the element will be processed again.
-	waitOnce,			// Whether to stop after the first elements are found.
-	interval,			// The time (ms) to wait between iterations.
-	maxIntervals,		// The max number of intervals to run (negative number for unlimited).
+  selectorOrFunction, // A selector string, a list of selector strings, or a function.
+  callback,			// The callback function; Takes a dom element and a selector string as parameters. If it returns true, the element will be processed again.
+  waitOnce,			// Whether to stop after the first elements are found.
+  interval,			// The time (ms) to wait between iterations.
+  maxIntervals,		// The max number of intervals to run (negative number for unlimited).
 ) => {
-    waitOnce = waitOnce === undefined ? true : waitOnce;
-    interval = interval === undefined ? 300 : interval;
-    maxIntervals = maxIntervals === undefined ? -1 : maxIntervals;
+  waitOnce = waitOnce === undefined ? true : waitOnce;
+  interval = interval === undefined ? 300 : interval;
+  maxIntervals = maxIntervals === undefined ? -1 : maxIntervals;
 
-	const targetNodes = getTargetNodes(selectorOrFunction);
+  const targetNodes = getTargetNodes(selectorOrFunction);
  
-	if (targetsFound = targetNodes.length > 0) {
-		targetNodes.forEach(([selector, node]) => {
-			if (!node.getAttribute(FOUND_ATTR)) {
-				targetsFound = callback(node, selector) ? false : !node.setAttribute(FOUND_ATTR, true);
-			}
-		});
-	}
+  if (targetsFound = targetNodes.length > 0) {
+    targetNodes.forEach(([selector, node]) => {
+      if (!node.getAttribute(FOUND_ATTR)) {
+        targetsFound = callback(node, selector) ? false : !node.setAttribute(FOUND_ATTR, true);
+      }
+    });
+  }
  
-	if (maxIntervals !== 0 && !(targetsFound && waitOnce)) {
-		setTimeout(() => waitForKeyElements(selectorOrFunction, callback, waitOnce, interval, maxIntervals - 1), interval);
-	}
+  if (maxIntervals !== 0 && !(targetsFound && waitOnce)) {
+    setTimeout(() => waitForKeyElements(selectorOrFunction, callback, waitOnce, interval, maxIntervals - 1), interval);
+  }
 };
