757186bb9b6cb98de69efe51955f464b64ead90f
Maxwell Spiegelman <maxwellspiegelman@gmail.com>
2024-10-19 09:08:59 -0400

    Use GM_xmlhttpRequest for dl util

--- utils/downloadAcrossOrigin.js
+++ utils/downloadAcrossOrigin.js
@@ -1,24 +1,37 @@
 // A utility function for downloading url based files across origins
 
-const downloadAcrossOrigin = (
-  url,      // URL of the file to download as a string
-  callback, // Function to call once download has started
-) => {
-  const ajax = new XMLHttpRequest();
-  ajax.open("GET", url, true);
-  ajax.responseType = 'blob';
-  ajax.onload = e => {
-    const anchor = Object.assign(document.createElement("a"), {
-      href: window.URL.createObjectURL(e.target.response),
-      download: url.split("/").pop().split("?")[0],
-      hidden: true,
-      onclick: e => e.stopPropagation()
+const downloadAcrossOrigin = ({
+    url,                    // URL of the file to download as a string
+    headers = {},           // Headers to send with the XHR
+    callback,               // Function to call once download has started
+    filename,               // Name of the file to be saved
+    showProgress = false,   // Whether or not to show progress in console
+}) => {
+    if (!GM_xmlhttpRequest) {
+        return console.error('You must have the GM_xmlhttpRequest @grant in you user script');
+    }
+
+    GM_xmlhttpRequest({
+        method: "GET",
+        url,
+        headers,
+        responseType: 'blob',
+        onload: e => {
+            const anchor = Object.assign(document.createElement("a"), {
+                href: window.URL.createObjectURL(e.response),
+                download: filename || 'download.mp4',
+                hidden: true,
+                onclick: e => e.stopPropagation()
+            });
+            document.body.appendChild(anchor);
+            anchor.click();
+            document.body.removeChild(anchor);
+            callback && callback(e);
+            setTimeout(() => window.URL.revokeObjectURL(anchor.href), 250);
+        },
+        onerror: e => console.log('Error: ', e),
+        onprogress: showProgress ? ({ total, done }) => {
+            console.log('Progress: ', `${Math.round((done / total) * 10000) / 100}%`);
+        } : () => {},
     });
-    document.body.appendChild(anchor);
-    anchor.click();
-    document.body.removeChild(anchor);
-    callback && callback();
-    setTimeout(() => window.URL.revokeObjectURL(anchor.href), 250);
-  };
-  setTimeout(() => ajax.send(), 0);
 };
