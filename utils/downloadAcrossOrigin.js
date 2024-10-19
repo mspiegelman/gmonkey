0ae046528a2c77b7fca2a096fcd645d61960e174
Maxwell Spiegelman <maxwellspiegelman@gmail.com>
2024-10-19 00:45:42 -0400

    Add download and waitForElements util functions

---
+++ utils/downloadAcrossOrigin.js
@@ -0,0 +1,24 @@
+// A utility function for downloading url based files across origins
+
+const download = (
+    url,      // URL of the file to download as a string
+    callback, // Function to call once download has started
+) => {
+    const ajax = new XMLHttpRequest();
+    ajax.open("GET", url, true);
+    ajax.responseType = 'blob';
+    ajax.onload = e => {
+        const anchor = Object.assign(document.createElement("a"), {
+            href: window.URL.createObjectURL(e.target.response),
+            download: url.split("/").pop().split("?")[0],
+            hidden: true,
+            onclick: e => e.stopPropagation()
+        });
+        document.body.appendChild(anchor);
+        anchor.click();
+        document.body.removeChild(anchor);
+        callback && callback();
+        setTimeout(() => window.URL.revokeObjectURL(anchor.href), 250);
+    };
+    setTimeout(() => ajax.send(), 0);
+};
---
+++ utils/waitForElements.js
@@ -0,0 +1,35 @@
+// A utility function for userscripts that detects and handles AJAXed content.
+
+const FOUND_ATTR = 'data-userscript-alreadyFound';
+
+const getTargetNodes = selector => {
+	if (typeof selector === 'function') return (selector() || []).map(n => [selector.name, n]);
+	if (typeof selector === 'string') return [...document.querySelectorAll(selector)].map(n => [selector, n]);
+	return selector.flatMap(s => [...document.querySelectorAll(s)].map(n => [s, n]));
+};
+
+const waitForKeyElements = (
+	selectorOrFunction, // A selector string, a list of selector strings, or a function.
+	callback,			// The callback function; Takes a dom element and a selector string as parameters. If it returns true, the element will be processed again.
+	waitOnce,			// Whether to stop after the first elements are found.
+	interval,			// The time (ms) to wait between iterations.
+	maxIntervals,		// The max number of intervals to run (negative number for unlimited).
+) => {
+    waitOnce = waitOnce === undefined ? true : waitOnce;
+    interval = interval === undefined ? 300 : interval;
+    maxIntervals = maxIntervals === undefined ? -1 : maxIntervals;
+
+	const targetNodes = getTargetNodes(selectorOrFunction);
+ 
+	if (targetsFound = targetNodes.length > 0) {
+		targetNodes.forEach(([selector, node]) => {
+			if (!node.getAttribute(FOUND_ATTR)) {
+				targetsFound = callback(node, selector) ? false : !node.setAttribute(FOUND_ATTR, true);
+			}
+		});
+	}
+ 
+	if (maxIntervals !== 0 && !(targetsFound && waitOnce)) {
+		setTimeout(() => waitForKeyElements(selectorOrFunction, callback, waitOnce, interval, maxIntervals - 1), interval);
+	}
+};
